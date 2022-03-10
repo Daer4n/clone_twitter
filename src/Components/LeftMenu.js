e122d9864d71981ee8a5d7741bb78fa06b0927a6
daer4n <fuzzebf@gmail.com>
2022-03-10 12:00:36 +0100

    :lipstick: update du last btn du menu de gauche

--- src/Components/LeftMenu.js
+++ src/Components/LeftMenu.js
@@ -9,7 +9,7 @@ export const LeftMenu = () => {
 				<li><a href="#" onClick><span className="icon home"></span>Accueil</a></li>
 				<li><a href="#" onClick><span className="icon signets"></span>Signets</a></li>
 				<li><a href="#" onClick><span className="icon profil"></span>Profil</a></li>
-				<li><a href="#" onClick>Touiter</a></li>
+				<li><a href="#" className="send" onClick>Touiter</a></li>
 			</ul>
 			<div>
 				<span className="profile"></span>
